b742c08eeeebeb9b697e0f615f31638490fce6c5
Leliarkelly <leliarkelly41@gmail.com>
2021-09-20 19:35:50 +0000

    further commits

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// $(document).ready(function(){
-//     $('.datepicker').datepicker();
-//   });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
 
-
+  const instance = M.FormSelect.getInstance(elem);
+  console.log(instance);
+       
