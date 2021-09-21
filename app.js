f6685f10ea6c930004e42dcb90461ed65bdaddc3
Leliarkelly <leliarkelly41@gmail.com>
2021-09-21 14:30:46 +0000

    UI completed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+//   });
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+//   $(document).ready(function(){
+//     $('select').formSelect();
+//   });
 
-  const instance = M.FormSelect.getInstance(elem);
-  console.log(instance);
+//   const instance = M.FormSelect.getInstance(elem);
+//   console.log(instance);
        
