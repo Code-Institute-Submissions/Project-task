48875893e8d6e9f26401710fe06c3f3b2341e369
Leliarkelly <leliarkelly41@gmail.com>
2021-09-22 16:24:06 +0000

    changes

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('select');
-//     var instances = M.FormSelect.init(elems, options);
-//   });
 
-//   $(document).ready(function(){
-//     $('select').formSelect();
-//   });
-
-//   const instance = M.FormSelect.getInstance(elem);
-//   console.log(instance);
        
