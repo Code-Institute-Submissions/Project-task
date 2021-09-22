4a91819378ecb6ee839b91db1314e03c7a109c56
Leliarkelly <leliarkelly41@gmail.com>
2021-09-22 18:32:31 +0000

    add css

--- app.js
+++ app.js
@@ -3,21 +3,45 @@ const inputTask = document.getElementById('task');
 const inputTasktype = document.getElementById('task-type');
 const inputDate = document.getElementById('target-date');
 
+
+function changebackgroundcolor(){
+    formSection.className = ''
+}
+
+changebackgroundcolor();
+
 function showError(input, message) {
-    const formControl = input.parentElement;
-    formSection.className = 'formSection error';
+    const formSection = input.parentElement;
+    formSection.className = 'formSection.error input';
     const small = formSection.querySelector('small');
     small.innerText = message;
-    console.log(message);
-  }   
+ }   
 
   // Show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+    const formSection = input.parentElement;
+    formSection.className = 'formSection.success input';
   }
-//add event listener
 
-//   form.addEventListener('submit', function(e) {
-//     e.preventDefault();
-// });
+// Event Listeners
+
+form.addEventListener('submit', function(e) {
+     e.preventDefault();
+if (inputTask.value === ''){
+    showError(task,  'task is required');
+}else {
+    showSuccess(task);
+}
+
+if (inputTasktype.value === ''){
+    showError(task-type,  'task type is required');
+}else {
+    showSuccess(task-type);
+}
+
+if (inputDate.value === ''){
+    showError(target-date,  'target date is required');
+}else {
+    showSuccess(target-date);
+}
+ });
