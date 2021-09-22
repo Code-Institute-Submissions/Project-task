7a7511bc7506b1788f054e7cbc7d105b9a221b74
Leliarkelly <leliarkelly41@gmail.com>
2021-09-22 17:24:46 +0000

    add error message

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ const inputTask = document.getElementById('task');
 const inputTasktype = document.getElementById('task-type');
 const inputDate = document.getElementById('target-date');
 
-    
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formSection.className = 'formSection error';
+    const small = formSection.querySelector('small');
+    small.innerText = message;
+    console.log(message);
+  }   
+
+  // Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+  }
+//add event listener
+
+//   form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+// });
