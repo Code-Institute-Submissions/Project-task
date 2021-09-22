e06466b133cd15310ba3d949e69da70a5ed267e3
Leliarkelly <leliarkelly41@gmail.com>
2021-09-22 16:41:24 +0000

    add UI

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+const form = document.getElementById('form');
+const inputTask = document.getElementById('task');
+const inputTasktype = document.getElementById('task-type');
+const inputDate = document.getElementById('target-date');
 
        
