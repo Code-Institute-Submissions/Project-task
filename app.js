89541c543e6af35b57f386d36858568ebfd974b6
Leliarkelly <leliarkelly41@gmail.com>
2021-09-29 10:38:11 +0000

    after tidy up edit

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const inputTask = document.getElementById('task');
-const inputTeammember = document.getElementById('team-member');
-const inputDate = document.getElementById('target-date');
-const taskList = document.querySelector('.task-list');
-const tasklistdata = [];
-const taskButton = document.getElementById('addtaskbtn');
-const cleartasks = document.getElementById('cleartasksbtn');
+const tasklistdata = [];  //that will hold all the value of task list
+const inputTask = document.getElementById('task'); //first input field 
+const inputTeammember = document.getElementById('team-member'); //2nd input field
+const inputDate = document.getElementById('target-date'); //3rd input field which is that target date for the task
+const taskList = document.querySelector('.task-list');// tasks will be appended inside the tasklist table
+const taskButton = document.getElementById('addtaskbtn');  // button that will add task record to the task list
+const cleartasks = document.getElementById('cleartasksbtn') //button that will delete all task deleted
 
 
-// Show error message
+//function to show error and add red border when no information is inputted into field
+
 function showError(input, message) {
-    const formInput = input;
-    formInput.style.border = "thick solid #e74c3c";
-    const small = formInput.querySelector('small');
+    const formSection = input.parentElement;
+    formSection.className = 'form-section error';
+    const small = formSection.querySelector('small');
     small.innerText = message;
-     }   
-  // Show success outline
+ }   
+
+// function to show success that add classname success which add a greenborder and a tick
 function showSuccess(input) {
-    // const formSection = input.parentElement;
-    // formSection.className = 'form-section success';
-    const formInput = input;
-    formInput.style.border = "thick solid #2ecc71";    
+    const formSection = input.parentElement;
+    formSection.className = 'form-section success';
   }
 
-// Event Listeners
+// Event Listeners that intialise error or success functions when user moves away task field
 inputTask.addEventListener("blur", function(e) {
     if (inputTask.value === ''){
     showError(inputTask,  'task is required');
@@ -32,6 +32,7 @@ inputTask.addEventListener("blur", function(e) {
 }
  });
 
+ // Event Listeners that intialise error or success functions when user moves away team member field
  inputTeammember.addEventListener("blur", function(e) {
     if (inputTeammember.value === ''){
     showError(inputTeammember,  'team member is required');
@@ -39,6 +40,8 @@ inputTask.addEventListener("blur", function(e) {
     showSuccess(inputTeammember);
 }
  });
+
+ // Event Listeners that intialise error or success functions when user moves away from data input field
 
 inputDate.addEventListener("blur", function(e) {
     if (inputDate.value === ''){
@@ -48,19 +51,17 @@ inputDate.addEventListener("blur", function(e) {
 }
  });
 
-
-
-// add an eventListener on form, and listen for submit event
+// add an eventListener on form, and listen to the add task event
 taskButton.addEventListener('click', function(e) {
-  // prevent the page from reloading when submitting the form
+  // stops the page from reloading when submitting the form
   e.preventDefault();
   addTask(inputTask.value, inputTeammember.value, inputDate.value);
-   // call addTask function with input box current value
+   // call addTask function with add results from 3 fields
 });
 
-// function to add todo
+// function to addtask
 function addTask(item, teammember, target) {
-  // if item is not empty
+  // if fields are not empty
   if (item !== '' && teammember !== '' && target !== '') {
     // make a task object, which has id, name, and completed properties
     const today = new Date();
@@ -73,50 +74,55 @@ function addTask(item, teammember, target) {
       date: target.toDateString()
       
     }
-    console.log(inputDate.value);
+    
     // then add it to task array
     tasklistdata.push(taskitem);
     
- rendertasklist(tasklistdata); // then renders them between <ul>
- 
-   // finally clear the input box value
+ rendertasklist(tasklistdata); // then renders them to the row in the table
+
+   // finally clear the input box value as well as make add focus to the first field
     inputTask.value = '';
     inputDate.value = '';
     inputTeammember.value = '';
     inputTask.focus();
-    
-}
+   
+  
+  }
 else {swal("Whoops" ,  "Please complete all fields in the form!" ,  "error" );
 }
 }
 
-// function to render given tasks to screen
+// function to add given tasks to table
 function rendertasklist(tasklistdata) {
-  // clear everything inside <ul> with class=todo-items
+  // clear everything inside table list
   taskList.innerHTML = '';
 
-  // run through each item inside tasklist
+  // run through each item inside tasklist and add a new row
   tasklistdata.forEach(function(item, teammember, target) {
     const tr= document.createElement('tr');
-  
-    tr.innerHTML =
-     `<td><label><input type="checkbox" id="xxx"></label></td>
-     <td>${item.createdate}</td> 
-     <td>${item.name}</td>
+      tr.innerHTML =
+      
+      `<td><label><input type="checkbox" id="taskcheckbox" onclick="markComplete();"></label></td>
+      <td>${item.createdate}</td>
+      <td>${item.name}</td>
       <td>${item.member}</td>
       <td>${item.date}</td>
+      
       <td><button type="button" class ="btn btn-danger btn-sm float-end">X</button></td>
       
-  `;
-    // finally add the <li> to the <ul>
+    `;
+    // finally add the row to the end of the table
     taskList.append(tr);
+    // const checkbox = document.getElementById('taskcheckbox')
+    // console.log(taskcheckbox);
+
   });
 }
 
-//remove tasks
+//remove tasks listener on button click
 taskList.addEventListener('click', removeTask);
 
-//clear all tasks
+//clear all tasks listener on button click
 cleartasks.addEventListener('click', clearTasks);
 
 //remove task
@@ -136,6 +142,14 @@ function clearTasks(){
 }
 }
 
+function markComplete(){
+const checkBox = document.getElementById('taskcheckbox');
+console.log(checkBox);
+  if (checkBox.checked == true){
+    console.log("this is great")
+   } else {
+     console.log("no line through");
+  }
 
 
-
+}
