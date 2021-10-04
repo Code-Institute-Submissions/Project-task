fe1c65ca4d812aa94b8b81067ec43631e4fdcdd2
Leliarkelly <leliarkelly41@gmail.com>
2021-10-04 17:14:12 +0000

    final edits

--- README.md
+++ README.md
@@ -11,13 +11,14 @@ The Aim of the app is to create a simple task list with the following functional
 
 4) A user will only be able to add tasks when all fields have been completed.
 
-5) A user will be able to sort the lists by team member, task and target date.
+5) A user will be able to sort the lists so the latest tasks appears at the top of list.
+
 
 Specification:
 
 1) Created the UI using Html and CSS
 
-2) Created the following event listeners on the form:
+2) Created the following function on the form:
 
 3) Created showError() to display red border and error message if user failed to input into field
 
@@ -27,27 +28,12 @@ Specification:
 
 6) Create addTask function that creates taskitem array of all the input values and pushes to the tasklistdata array.  The data from that array is then added to UI into table row format.   The delete button and checkbox is added to each new row added
 
+7) RemoveClasses() that removes the success class from the input divs
+
 7) removeTask() that delete selected task on delete button click
 
 8) clearTask() that removes all tasks on clear task button click
 
 9) completeTask() that toggles between strikethrough css on checkbox click
 
-
-
-6) 
-
-
-Use forEach() to loop and output user/wealth
-Use map() to double wealth
-Use filter() to filter only millionaires
-Use sort() to sort by wealth
-Use reduce() to add all wealth
-    
-
-
-1) Create an array that will hold all the values of the table. 
-
-2) Each task item (task item, team member and target date) will be an object with three properties: 
-
-3)  
+10) sortbyDate() that sorts table by Target date
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const taskButton = document.getElementById('addtaskbtn');  // button that will a
 const cleartasks = document.getElementById('cleartasksbtn');
 const sortbtn = document.getElementById('sortbtn');
 
-//credits go to Brad Travesy 
+//******credits go to Brad Travesy at Vanilla project particularly the form validator project//
 
 //******function to show error and add red border when no information is inputted into field****
 
@@ -23,7 +23,7 @@ function showSuccess(input) {
     const formSection = input.parentElement;
     formSection.className = 'form-section success';
   }
-  //*******/ remove success class on form-section class*******************
+//*******/ remove success class on form-section class*******************
  
   function removeClasses(formSection) {
   for (var i = 0; i < formSection.length; i++) {
@@ -61,20 +61,20 @@ inputDate.addEventListener("blur", function(e) {
 
 // add an eventListener listen to the add task event
 taskButton.addEventListener('click', function(e) {
-  // stops the page from reloading when submitting the form
+// stops the page from reloading when submitting the form
   e.preventDefault();
   addTask(inputTask.value, inputTeammember.value, inputDate.value);
-  // remove success class from div elements
+// remove success class from div elements
   formSection = document.getElementsByClassName("form-section")
   removeClasses(formSection);
-   // call addTask function with add results from 3 fields
+// call addTask function with add results from 3 fields
 });
 
 // function to addtask
 function addTask(item, teammember, target) {
-  // if fields are not empty
+// if fields are not empty
   if (item !== '' && teammember !== '' && target !== '') {
-    // make a task object, which has id, name, and completed properties
+// make a task object, which has id, name, and completed properties
     const target = new Date(inputDate.value)
     const taskitem = {
       name: item,
@@ -82,18 +82,18 @@ function addTask(item, teammember, target) {
       date: target.toDateString(),
       raw_date:inputDate.value,    
     }
-    
-    // then add it to task array
+
+// then add it to task array
     taskListdata.push(taskitem);
     
- rendertasklist(taskListdata); // then adds them to the row in the table
-   // finally clear the input box value
+    rendertasklist(taskListdata); // then adds them to the row in the table
+// finally clear the input box value
     inputTask.value = '';
     inputDate.value = '';
     inputTeammember.value = '';
       }
-    //nice delete button from SWAL framework
-else {swal("Whoops" ,  "Please complete all fields in the form!" ,  "error" );
+//nice delete confirm message I got from SWAL framework
+    else {swal("Whoops" ,  "Please complete all fields in the form!" ,  "error" );
 }
 }
 
@@ -105,14 +105,14 @@ function rendertasklist(taskListdata) {
   taskListdata.forEach(function(item, teammember, target) {
     const tr= document.createElement('tr');
       tr.innerHTML = 
-      `<td class = "col-xs-3" style = "display:None">${item.raw_date}</td>  
-      <td class ="col-xs-1"><input type="checkbox" style = "margin-left:15%" class ="checkbox" id="taskcheckbox" onclick="myFunction()"</td>
+     `<td class = "col-xs-3" style = "display:None">${item.raw_date}</td>  
+      <td class ="col-xs-1"><input type="checkbox" style = "margin-left:15%" class ="checkbox" id="taskcheckbox" onclick="completeTask()"</td>
       <td class = "col-xs-7">${item.name}</td>
       <td class = "col-xs-2"style = "padding-left:15%" >${item.member}</td>
       <td class = "col-xs-2">${item.date}
       <button type="button" class ="btn btn-danger btn-sm float-end">X</button></td>
          `;
-    // finally add the row to the end of the table
+// finally add the row to the end of the table
     taskList.append(tr);
   });
 }
@@ -122,7 +122,6 @@ taskList.addEventListener('click', removeTask);
 
 function removeTask(e){
   if (e.target.classList.contains('float-end')){
- 
   value = e.target.parentElement.previousElementSibling.innerText
   console.log(value)
   for (i in taskListdata){
@@ -136,22 +135,21 @@ function removeTask(e){
 }
 //*****************toggle between complete and uncomplete strikethrough********************
 
-function myFunction(){
+function completeTask(){
   const checkBox = document.getElementById('taskcheckbox');
   checkBox.parentElement.parentElement.classList.toggle("strikethrough");
 }
 
-
 //*******************/ function to Sort date by Target date*****************
+//credit goes to support from Sardar Dawar a software developer and mentor who provided me with the sort functionality below
  function convertDate(d){
   var p = d.split("-")
   return +(p[0]+p[1]+p[2])
 }
-
  sortbtn.addEventListener("click", sortByDate)
  function sortByDate() {
   var tbody = document.querySelector("#tasklist tbody");
-  // get trs as array for ease of use
+// get trs as array for ease of use
   var rows = [].slice.call(tbody.querySelectorAll("tr"));
   rows.sort(function(a,b) {
     return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
@@ -161,12 +159,10 @@ function myFunction(){
   });
 }
 
-  // *************** Sort Function *******************
 
 
-// add an eventListener listen to the clear task event
+//***** */ add an eventListener listen to the clear task event*******
 cleartasks.addEventListener('click', function(e) {
-  //clear task function
   if(confirm('are you sure you want to delete all tasks')){
     for(i in taskListdata){
       taskListdata.splice(i)
